104163aaf55bf7e2972e5b0adf51dafd8ab1ca7a
Yunze <yunze.moe@outlook.com>
2016-05-02 17:43:36 +0800

    server improving...

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
+var path = require('path');
 var express = require('express');
 
-var app = express();
-app.listen(10088);
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var MongoStore = require('connect-mongo')(session);
 
-app.post('/', function(req, res) {
-  res.send('Hello World!');
-});
+var server = express();
+server.listen(10088);
+
+server.use(session({
+  secret: 'Akagi.moe',
+  store: new MongoStore({
+    url: 'mongodb://localhost/BSP',
+    ttl: 1 * 24 * 60 * 60
+  })
+}));
+
+server.use(bodyParser.json());
+server.use(express.static(__dirname + "/../static"));
