0a5d72198b2810718a3df78d1b1cdd0bb6f38752
Yunze <yunze.moe@outlook.com>
2016-05-24 15:19:38 +0800

    test

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # BSP
 A project for B/S assignment in ZJU 2016
+
+### Building
+
+``` bash
+npm install
+# watch:
+npm run dev
+# build:
+npm run build
+```
---
+++ src/components/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <p>{{data}}, {{name}}!</p>
+</template>
+
+<script>
+  export default {
+
+    name: 'Main',
+
+    data () {
+      return {
+        data: 'Hello World'
+      }
+    },
+
+    computed: {
+      name () {
+        return 'Yunze'
+      }
+    }
+
+  }
+</script>
+
+<style lang="stylus">
+</style>
