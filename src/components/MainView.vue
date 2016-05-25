1600f51f2879e2a495612a36230201013404bbb0
Yunze <yunze.moe@outlook.com>
2016-05-25 23:26:55 +0800

    init main view

---
+++ src/components/MainView.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Hello!</h1>
+  <h1>{{id}}</h1>
+</template>
+
+<script>
+  export default {
+
+    name: "Main",
+
+    data () {
+      return {
+        id: ""
+      }
+    },
+
+    route: {
+      data ({ to }) {
+        let id = to.params.user_id;
+        return {
+          id: id
+        }
+      }
+    }
+
+  }
+</script>
+
+<style lang="stylus">
+  @import '~vux/vux.css'
+</style>
