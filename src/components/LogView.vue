994827d1f1ee5dc124912bd1b80624208ec648d5
Yunze <yunze.moe@outlook.com>
2016-05-25 23:26:04 +0800

    add log and register view

---
+++ src/components/LogView.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div id="icon-div">
+      <icon name="weixin" scale="5"></icon>
+    </div>
+    <group>
+      <x-input title="账号" placeholder="请输入登录邮箱" is-type="email" :value.sync="mail" v-ref:mail></x-input>
+      <x-input title="密码" placeholder="请输入密码" :min="8" :max="32" :value.sync="password" v-ref:password></x-input>
+    </group>
+    <group>
+      <flexbox>
+        <flexbox-item>
+          <x-button type="primary" @click="login">登录</x-button>
+        </flexbox-item>
+        <flexbox-item>
+          <x-button @click="register">注册</x-button>
+        </flexbox-item>
+      </flexbox>
+    </group>
+    <p>{{mail}}</p>
+    <br />
+    <p>{{password}}</p>
+  </div>
+</template>
+
+<script>
+  import XInput from 'vux/components/x-input'
+  import Group from 'vux/components/group'
+  import XButton from 'vux/components/x-button'
+  import Flexbox from 'vux/components/flexbox'
+  import FlexboxItem from 'vux/components/flexbox-item'
+  import Icon from 'vue-awesome/dist/vue-awesome'
+
+  export default {
+
+    name: "Login",
+
+    components: {
+      XInput,
+      Group,
+      XButton,
+      Flexbox,
+      FlexboxItem,
+      Icon
+    },
+
+    data () {
+      return {
+        mail: "",
+        password: ""
+      }
+    },
+
+    methods: {
+      register () {
+        this.$route.router.go({name: "register"});
+      },
+      login () {
+        this.$route.router.go({name: "main", params: {user_id: this.mail}});
+      }
+    }
+
+  }
+</script>
+
+<style lang="stylus">
+  @import '~vux/vux.css'
+
+  #icon-div
+    margin-top 10px
+    text-align center
+    color #228B22
+</style>
---
+++ src/components/RegisterView.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <group title="账户信息">
+      <x-input title="邮箱" placeholder="注册邮箱" is-type="email" :value.sync="mail"></x-input>
+      <x-input title="密码" placeholder="输入密码" :min="8" max="32" :value.sync="password"></x-input>
+      <x-input title="确认" placeholder="确认密码" :min="8" max="32" :equal-with="password"></x-input>
+    </group>
+    <group title="个人信息">
+      <x-input title="昵称" placeholder="输入昵称" :value.sync="nickname"></x-input>
+      <x-input title="手机" placeholder="输入手机号码" keyboard="number" is-type="china-mobile" :value.sync="phone"></x-input>
+    </group>
+    <br />
+    <x-button type="primary" @click="doRegister">注册新用户</x-button>
+  </div>
+</template>
+
+<script>
+  import Group from 'vux/components/group'
+  import XInput from 'vux/components/x-input'
+  import XButton from 'vux/components/x-button'
+
+  export default {
+
+    name: 'Register',
+
+    components: {
+      Group,
+      XInput,
+      XButton
+    },
+
+    data () {
+      return {
+        mail: "",
+        password: "",
+        nickname: "",
+        phone: ""
+      }
+    },
+
+    methods: {
+      doRegister () {
+        this.$route.router.go({name: 'login'});
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import '~vux/vux.css'
+</style>
